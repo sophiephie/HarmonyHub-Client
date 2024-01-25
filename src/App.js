56fdbcc846ef34ce31e197690e119898ea22a02a
DrSloughKeg <bpye1995@gmail.com>
2024-01-25 14:26:46 -0500

    First! placeholder home page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WELCOME TO HARMONY HUB</h1>
+      <h2>PlACE HOLDER</h2>
+    </div>
+  );
+}
+
+export default App;
