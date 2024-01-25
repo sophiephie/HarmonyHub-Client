75470b466bcb453eeb76ebd1f876cbc7ca7f919c
DrSloughKeg <bpye1995@gmail.com>
2024-01-25 15:15:14 -0500

    bare bones home page/navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
       <h1>WELCOME TO HARMONY HUB</h1>
-      <h2>PlACE HOLDER</h2>
+      <h2>PLACE HOLDER</h2>
+      <Router>
+        <div className="navbar">
+          <Link to="/">Home</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="outer">
+      <div className="card">
+        <h2>Harmony Hub is a work in progress.</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
