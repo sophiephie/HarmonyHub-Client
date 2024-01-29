c6241006d25b27f90c9e607fc6a93b45ebe9c2ed
Sophie Hsu <sophie.hsu@johnabbottcollege.net>
2024-01-29 13:54:48 -0500

    update folder structure for navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Route, Routes, Link, useNavigate } from "react-router-dom";
 import Logo from "./assets/logo-no-background.png";
 import { GoogleLogin } from "@react-oauth/google";
-import Home from "./pages/home";
-import SignUpPage from "./pages/signup";
-import Dashboard from "./pages/userDash";
+import Home from "../pages/home";
+import SignUpPage from "../pages/signup";
+import Dashboard from "../pages/userDash";
 import axios from "axios";
 
 const NavBar = () => {
