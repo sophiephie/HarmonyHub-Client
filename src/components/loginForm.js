75d8e3e6fc46700cbdb6d84bec38e280d73327ab
Sophie Hsu <sophie.hsu@johnabbottcollege.net>
2024-01-29 14:58:49 -0500

    add a create account button

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -76,6 +76,13 @@ const LoginForm = ({ onLoginSuccess }) => {
       >
         Login
       </button>
+      <button
+        type="button"
+        className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 bg-blue-300"
+        onClick={() => onLoginSuccess(true)}
+      >
+        Create an Account
+      </button>
     </form>
   );
 };
