69407938f0bb0b8e7dfb3f8b7b5b5d9bc52092d7
NComGit <ncomthrowaway@protonmail.com>
2024-01-29 19:44:23 -0500

    Fixed styling(temp) and routing for admin dash

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import "./styles.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navBar";
+import Dashboard from './pages/adminDashboard';
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
       <GoogleOAuthProvider clientId="747162908197-oa2drb4bgh86gn4sc9jg72q4hv83h2f9.apps.googleusercontent.com">
         <Router>
           <NavBar />
+          <Routes>
+            <Route path="/admin/" element={<Dashboard />} />
+          </Routes>
+
         </Router>
       </GoogleOAuthProvider>
     </div>
